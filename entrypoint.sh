298eb44ffab65f6c6e912ac16013ea94a918caa7
Štefan Ľupták <stefan.luptak@smarkup.com>
2019-08-20 15:37:50 +0200

    Workaround until hub 2.12.3 is available

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 # hub release create [-dpoc] [-a <FILE>] [-m <MESSAGE>|-F <FILE>] [-t <TARGET>] <TAG>
+
+echo "github.com:\n- user: $GITHUB_USER\n  oauth_token: $GITHUB_TOKEN\n  protocol: https" > ~/.config/hub
+unset GITHUB_TOKEN
+unset GITHUB_USER
+
 hub version
 hub release create $*
