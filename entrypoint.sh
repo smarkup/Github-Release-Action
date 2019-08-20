035bb778eafa5a8db72aa52bce401c24e6a61893
Štefan Ľupták <stefan.luptak@smarkup.com>
2019-08-20 14:57:48 +0200

    Use more generic hub call

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 # hub release create [-dpoc] [-a <FILE>] [-m <MESSAGE>|-F <FILE>] [-t <TARGET>] <TAG>
-# hub release create $*
-MESSAGE=$*
-hub release create -m ${MESSAGE} $(date +%Y%m%d%H%M%S)
+hub release create $*
