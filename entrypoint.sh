9fe17560352ec022be287d2749ed3450cbc9b756
Štefan Ľupták <stefan.luptak@smarkup.com>
2019-08-21 14:52:00 +0200

    Add debug echos

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 # hub release create [-dpoc] [-a <FILE>] [-m <MESSAGE>|-F <FILE>] [-t <TARGET>] <TAG>
+echo "\$GITHUB_USER=$GITHUB_USER"
+echo "\$GITHUB_REF=$GITHUB_REF"
+
 hub version
 hub release create $*
