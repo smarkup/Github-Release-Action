2f3897d69ac6c0192f336bce4bff5669b92238dc
Štefan Ľupták <stefan.luptak@smarkup.com>
2019-08-21 15:26:51 +0200

    Fix slash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # hub release create [-dpoc] [-a <FILE>] [-m <MESSAGE>|-F <FILE>] [-t <TARGET>] <TAG>
-GIT_TAG=$(echo $GITHUB_REF | cut -d '\' -f 3)
+GIT_TAG=$(echo $GITHUB_REF | cut -d '/' -f 3)
 echo "\$GITHUB_USER=$GITHUB_USER"
 echo "\$GITHUB_REF=$GITHUB_REF"
 echo "\$GIT_TAG=$GIT_TAG"
