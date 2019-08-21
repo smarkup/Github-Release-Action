a2bd483758cc1b44950b95857a2277c3fa431540
Štefan Ľupták <stefan.luptak@smarkup.com>
2019-08-21 15:08:02 +0200

    Make the action more release create specific

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 # hub release create [-dpoc] [-a <FILE>] [-m <MESSAGE>|-F <FILE>] [-t <TARGET>] <TAG>
+GIT_TAG=$(echo $GITHUB_REF | cut -d '\' -f 3)
 echo "\$GITHUB_USER=$GITHUB_USER"
 echo "\$GITHUB_REF=$GITHUB_REF"
+echo "\$GIT_TAG=$GIT_TAG"
+echo "\$RELEASE_ASSET_PATH=$RELEASE_ASSET_PATH"
 
 hub version
-hub release create $*
+hub release create -a $RELEASE_ASSET_PATH -m $GIT_TAG $GIT_TAG
